6d1663d3e46b4dca7c1dbb8f356506531d220976
vococo-fkluge <felix@vococo.ca>
2026-02-26 10:22:16 -0700

    Add custom `Rating` control with theming support and integrate into `MainWindow`

---
+++ Styling/Theme/Logic/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Styling.Theme.Logic;
+
+public class Rating : ContentControl {
+    protected override Type StyleKeyOverride => typeof(Rating);
+
+    public static readonly StyledProperty<double> CircleSizeProperty = AvaloniaProperty.Register<Rating, double>(
+        nameof(CircleSize));
+    public double CircleSize {
+        get => GetValue(CircleSizeProperty);
+        set => SetValue(CircleSizeProperty, value);
+    }
+}
